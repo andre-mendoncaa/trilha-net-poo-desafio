b387b1c76ceca7e0a04579ca97591b14da54b461
Andre <andremendonca0504@gmail.com>
2023-12-31 10:22:18 -0300

    Realização do Desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("946335330", "K3", "350511111111-1", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone("11946335330", "Iphone 15", "18181818181818", 256);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Youtube");
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
